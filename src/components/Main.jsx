c5548b0e387538e9b41704782d92c81c3957ab2d
Roderick Fayson <rgfayson2@gmail.com>
2022-08-08 14:15:25 -0400

    props

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Main({name, isAmazing}) {
+    
+    return (
+        <>
+        <h1>Hello {props.name}</h1>
+        {isAmazing && <h2>Fruit: {fruit}</h2>}
+        </>
+        
+    )
+}
+
---
+++ src/components/Menubar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Menubar() {
+    return (
+        <main>
+            <h3>Menubar</h3>
+
+        </main>
+
+    )
+}
+
+export default Menubar;
